b7b22112b3e97036bac655bd30ae978520a521c0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-08-15 12:20:51 -0400

    Create ListProductsVariantsHelper.php

---
+++ system/modules/isotope_list_products_variants/library/Bcs/Backend/ListProductsVariantsHelper.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Bright Cloud Studio - Isotope List Products Variants
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-products-variants
+ * @link       http://brightcloudstudio.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+
+namespace SuperSort\Backend;
+
+
+use Isotope\Model\Product;
+
+
+class SuperSortHelper extends \Isotope\Backend
+{
+
+
+    /**
+     * Find all products we need to list.
+     *
+     * @return array
+     */
+    function getProducts(\Contao\DataContainer $dc)
+    {
+		$objProducts = Product::findPublishedByCategories(array((int)$dc->id));
+
+		$arrProducts = array();
+
+		if ($objProducts) {
+			while ($objProducts->next()) {
+				$arrProducts[$objProducts->id] = $objProducts->name ." " .$objProducts->color_or_design_name ." (SKU: " .$objProducts->sku .")";
+			}
+		}
+
+        return $arrProducts;
+    }
+
+}
