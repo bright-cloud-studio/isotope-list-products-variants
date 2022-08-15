68d4a5cb145870e25fe47f2cbd0149279be8f012
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-08-15 12:57:37 -0400

    Update ListProductsVariants.php

--- system/modules/isotope_list_products_variants/library/Bcs/Module/ListProductsVariants.php
+++ system/modules/isotope_list_products_variants/library/Bcs/Module/ListProductsVariants.php
@@ -19,20 +19,29 @@ use Isotope\Module\ProductList;
 class ListProductsVariants extends ProductList
 {
 
+	/* We are going to customize the compile to add Variants to template */
+	protected function compile()
+	{
+		$arrVariants = array();
+		$arrVariants[] = array(
+			'variant_1'     => 'ASDF',
+			'variant_2'     => 'QUERTY'
+		);
+		$this->Template->variants = $arrVariants;
+	}
+	
 	/**
-     * Find all products we need to list.
-     *
-     * @param array|null $arrCacheIds
-     *
-     * @return array
-     */
-    protected function findProducts($arrCacheIds = null)
-    {
-	global $objPage;
-
-	$arrProducts = parent::findProducts($arrCacheIds);
-
-        return $arrProducts;
-    }
+	* Find all products we need to list.
+	*
+	* @param array|null $arrCacheIds
+	*
+	* @return array
+	*/
+	protected function findProducts($arrCacheIds = null)
+	{
+		global $objPage;
+		$arrProducts = parent::findProducts($arrCacheIds);
+		return $arrProducts;
+	}
 
 }
