2d21cd8961265fa9ad0aff7d358f8993ab8df9ca
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-13 14:37:42 -0400

    Create tl_iso_product.php

---
+++ system/modules/isotope_list_products_variants/dca/tl_iso_product.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Add fields to tl_user */
+$GLOBALS['TL_DCA']['tl_iso_product']['fields']['size_as_decimal'] = array
+(
+	'label'			=> &$GLOBALS['TL_LANG']['tl_iso_product']['size_as_decimal'],
+	'inputType'		=> 'text',
+	'default'		=> '',
+	'search'		=> true,
+	'eval'			=> array('mandatory'=>false, 'tl_class'=>'w50'),
+	'sql'			=> "varchar(255) NOT NULL default ''"
+);
