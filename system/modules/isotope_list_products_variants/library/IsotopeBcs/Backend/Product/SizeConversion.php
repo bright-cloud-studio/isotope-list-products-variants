64f38d5d0102861bcbae054db5aa6fa0d3e54e0e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-20 11:24:10 -0400

    Update SizeConversion.php

--- system/modules/isotope_list_products_variants/library/IsotopeBcs/Backend/Product/SizeConversion.php
+++ system/modules/isotope_list_products_variants/library/IsotopeBcs/Backend/Product/SizeConversion.php
@@ -30,8 +30,49 @@ class SizeConversion extends \Backend
      * @return string
      * @throws \Exception
      */
-    public function convertSize($varValue, DataContainer $dc)
+    public function convertSize(DataContainer $dc)
     {
-        \Database::getInstance()->prepare('UPDATE tl_iso_product SET size_as_decimal=? WHERE id=?')->execute("Alias for " ."999", $dc->activeRecord->id);
+        
+        // starting point
+        $newPrice = 0;
+        $oldPrice = $dc->activeRecord->wp_size;
+        
+        // if this has commas dont even try
+        if(str_contains($oldPrice, ',')) {
+            
+            $newPrice = 1;
+            
+        } else {
+            // remove html quote
+            $cleaning = str_replace('&#34;','', $oldPrice);
+            // remove actual quote
+            $cleaning = str_replace('"','', $cleaning);
+            
+            
+            // this is a fraction
+            if(str_contains($cleaning, '/')) {
+                
+                // this is one of the weird */0 fractions
+                if(str_contains($cleaning, '/0')) {
+                    $cleaning = str_replace('/0', '',$cleaning);
+                    $cleaning = $cleaning * -1;
+                } else {
+                    $cleaning = $this->convertToDecimal($cleaning);
+                }
+            
+            } else {
+                $cleaning = $cleaning + 10000;
+            }
+            
+            $newPrice = $cleaning;
+        }
+        
+        \Database::getInstance()->prepare('UPDATE tl_iso_product SET size_as_decimal=? WHERE id=?')->execute($newPrice, $dc->activeRecord->id);
+    }
+
+    function convertToDecimal ($fraction)
+    {
+        $numbers=explode("/",$fraction);
+        return round($numbers[0]/$numbers[1],1);
     }
 }
