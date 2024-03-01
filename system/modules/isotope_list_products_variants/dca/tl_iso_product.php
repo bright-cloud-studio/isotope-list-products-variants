ff98f5e1526bbed03ba060515dca1b3b87e7fbe4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-01 14:04:36 -0500

    Update tl_iso_product.php

--- system/modules/isotope_list_products_variants/dca/tl_iso_product.php
+++ system/modules/isotope_list_products_variants/dca/tl_iso_product.php
@@ -1,14 +1,13 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_iso_product']['config']['onsubmit_callback'][] = array('IsotopeBcs\Backend\Product\SizeConversion', 'convertSize');
+//$GLOBALS['TL_DCA']['tl_iso_product']['config']['onsubmit_callback'][] = array('IsotopeBcs\Backend\Product\SizeConversion', 'convertSize');
 
 /* Add fields to tl_user */
-$GLOBALS['TL_DCA']['tl_iso_product']['fields']['size_as_decimal'] = array
-(
-	'label'			=> &$GLOBALS['TL_LANG']['tl_iso_product']['size_as_decimal'],
-	'inputType'		=> 'text',
-	'default'		=> '',
-	'search'		=> true,
-	'eval'			=> array('mandatory'=>false, 'tl_class'=>'w50'),
-	'sql'			=> "varchar(255) NOT NULL default ''"
-);
+//$GLOBALS['TL_DCA']['tl_iso_product']['fields']['size_as_decimal'] = array(
+//	'label'			=> &$GLOBALS['TL_LANG']['tl_iso_product']['size_as_decimal'],
+////	'inputType'		=> 'text',
+//	'default'		=> '',
+////	'search'		=> true,
+//	'eval'			=> array('mandatory'=>false, 'tl_class'=>'w50'),
+//	'sql'			=> "varchar(255) NOT NULL default ''"
+//);
