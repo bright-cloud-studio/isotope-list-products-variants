7c162067e68e8b0f46cc9742aa99d4157a27ede8
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-22 09:47:26 -0400

    Update autoload.php

--- system/modules/isotope_list_products_variants/config/autoload.php
+++ system/modules/isotope_list_products_variants/config/autoload.php
@@ -17,6 +17,7 @@ ClassLoader::addClasses(array
 	//'IsotopeBcs\Backend\Product\SizeConversion' 	=> 'system/modules/isotope_list_products_variants/library/IsotopeBcs/Backend/Product/SizeConversion.php',
 	'Bcs\Backend\ListProductsVariantsHelper' 	=> 'system/modules/isotope_list_products_variants/library/Bcs/Backend/ListProductsVariantsHelper.php',
 	'Bcs\Module\ListProductsVariantsList' 		=> 'system/modules/isotope_list_products_variants/library/Bcs/Module/ListProductsVariantsList.php',
+    'Bcs\Module\ModShowVariants' 		=> 'system/modules/isotope_list_products_variants/library/Bcs/Module/ModShowVariants.php',
 	'CustomTags\AddVariantsTags' 			=> 'system/modules/isotope_list_products_variants/library/CustomTags/AddVariantsTags.php'
 ));
 
@@ -26,5 +27,6 @@ ClassLoader::addClasses(array
 TemplateLoader::addFiles(array
 (
 	'item_variant' 	=> 'system/modules/isotope_list_products_variants/templates/items'
+    'mod_show_variants' 	=> 'system/modules/isotope_list_products_variants/templates/modules'
 ));
 
